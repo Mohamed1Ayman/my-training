c3df85c86e6d344d085f0c5fde53b0b44b437338
Mohamed Ayman <ma2078865@gmail.com>
2026-02-14 09:15:28 +0200

    complate the whole projet

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const message = document.getElementById('message');
 function addTask (){
     if(input.value=== ''){
     message.textContent = `You have to type any thing!`;
+    saveData()
     }else{
         let li = document.createElement('li');
         li.innerHTML = input.value;
@@ -14,9 +15,29 @@ function addTask (){
         span.innerHTML = '\u00d7';
         li.appendChild(span);
         message.textContent='';
+        saveData()
     }
+    
 }
 
 list.addEventListener("click",function(e){
-    
-})
+   if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+    saveData();
+   } else if(e.target.tagName === 'SPAN'){
+    e.target.parentElement.remove();
+    saveData();
+   }
+},false);
+
+function saveData(){
+    localStorage.setItem('data',list.innerHTML);
+}
+
+
+
+function showData() {
+    list.innerHTML = localStorage.getItem('data');
+}
+
+showData()
